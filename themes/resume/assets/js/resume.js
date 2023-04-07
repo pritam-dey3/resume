c376db3139083f1d6c5c2c4c27450b4e135b38ff
pritam-dey3 <pdeypritam@gmail.com>
2023-04-07 12:16:19 +0530

    improving sidebar wip

--- themes/resume/assets/js/resume.js
+++ themes/resume/assets/js/resume.js
@@ -2,7 +2,8 @@ const Sections = document.querySelector('#sections');
 const Body = document.querySelector('body');
 const Nav = document.querySelector('nav');
 
-Body.style.paddingTop = Nav.offsetHeight + 'px';
+Body.style.paddingTop = Nav.getBoundingClientRect().height + 'px';
+Sections.style.paddingTop = Nav.getBoundingClientRect().height + 'px';
 
 // Check if viewport is md or wider
 const mediaQuery = window.matchMedia('(min-width: 768px)');
@@ -30,9 +31,9 @@ function addPaddingToSections() {
 }
 
 // Run on window resize
-window.addEventListener('resize', handleMediaQuery);
-window.addEventListener('resize', addPaddingToSections);
+// window.addEventListener('resize', handleMediaQuery);
+// window.addEventListener('resize', addPaddingToSections);
 
-// Call the functions on page load to set initial padding
-handleMediaQuery();
-addPaddingToSections();
+// // Call the functions on page load to set initial padding
+// handleMediaQuery();
+// addPaddingToSections();
