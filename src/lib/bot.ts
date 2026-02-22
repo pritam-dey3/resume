f66e6e4f6623c298af52da4a04a3d194e4b58039
pritam-dey3 <pdeypritam@gmail.com>
2026-02-22 18:09:56 +0530

    minor fix

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import { fetchEventSource } from "@microsoft/fetch-event-source";
 import { v4 as uuidv4 } from "uuid";
 
